a0e17d598303e7796d74ece137f0884221a0c789
evilperfect <evilperfect87@gmail.com>
2017-04-26 13:02:42 +0800

    mount all NTFS partion once

--- mount.sh
+++ mount.sh
@@ -1,37 +1,53 @@
 #!/bin/bash
 
-if [ $# != 1 ]; then
-	echo "error!!"
-	echo "Usage: ntfsmount [disk_name]"
-	exit -1
-fi
+count=0
+for arg in `diskutil info -all | grep 'Volume Name' | cut -d : -f 2 | sed 's/^[[:space:]]*//' | sed 's/[ \t]*$//g'`
+do
+  volume_names[$count]=$arg
+  count=`expr $count + 1`
+done
 
-disk_name="$1"
-device_node=`diskutil info ${disk_name} | grep 'Device Node' | cut -d : -f 2 | sed 's/^[[:space:]]*//' | sed 's/[ \t]*$//g'`
 
-# check if it's a ntfs device
-fs_1=`diskutil info ${disk_name} | grep 'File System Personality' | cut -d : -f 2 | sed 's/^[[:space:]]*//' | sed 's/[ \t]*$//g'`
+#volume_names=`diskutil info -all | grep 'Volume Name' | cut -d : -f 2 | sed 's/^[[:space:]]*//' | sed 's/[ \t]*$//g'`
+tLen=${#volume_names[@]}
 
-fs_2=`diskutil info ${disk_name} | grep 'Type (Bundle)' | cut -d : -f 2 | sed 's/^[[:space:]]*//' | sed 's/[ \t]*$//g'`
+echo $tLen
 
-if [ "$fs_1" != "NTFS" ] && [ "$fs_2" != "ntfs" ]; then
-	echo "error!!"
-	echo "the given disk is an ${fs_1} device, not an NTFS device"
-	exit -1
-fi
+for (( i=0; i<${tLen}; i++ ));
+do
+	echo ${volume_names[$i]}
+    disk_name=${volume_names[$i]}
 
-mounted_path="/Volumes/${disk_name}"
-# echo "mounted_path is ${mounted_path}"
+    device_node=`diskutil info ${disk_name} | grep 'Device Node' | cut -d : -f 2 | sed 's/^[[:space:]]*//' | sed 's/[ \t]*$//g'`
+ 
+    # check if it's a ntfs device
+    fs_1=`diskutil info ${disk_name} | grep 'File System Personality' | cut -d : -f 2 | sed 's/^[[:space:]]*//' | sed 's/[ \t]*$//g'`
 
-sudo diskutil unmount "${mounted_path}"
+    fs_2=`diskutil info ${disk_name} | grep 'Type (Bundle)' | cut -d : -f 2 | sed 's/^[[:space:]]*//' | sed 's/[ \t]*$//g'`
 
-sudo mkdir "${mounted_path}"
-sudo mount -t ntfs -o rw,auto,nobrowse ${device_node} ${mounted_path}
-if [ $? -eq 0 ]; then
-	echo "mount success"
-	ln -s ${mounted_path} ~/Desktop/${disk_name}
-	echo "link to deskop"
-else
-	echo "mount error!!"
-	exit -1
-fi
+    if [ "$fs_1" != "NTFS" ] && [ "$fs_2" != "ntfs" ]; 
+    then
+	    echo "error!!"
+	    echo "the given disk is an ${fs_1} device, not an NTFS device"
+        #exit -1
+        continue
+    fi
+
+    mounted_path="/Volumes/${disk_name}"
+    # echo "mounted_path is ${mounted_path}"
+
+    sudo diskutil unmount "${mounted_path}"
+
+    sudo mkdir "${mounted_path}"
+    sudo mount -t ntfs -o rw,auto,nobrowse ${device_node} ${mounted_path}
+    if [ $? -eq 0 ]; then
+	    echo "mount success"
+	    ln -s ${mounted_path} ~/Desktop/${disk_name}
+	    echo "link to deskop"
+	    continue
+    else
+	    echo "mount error!!"
+	    #exit -1
+	    continue
+    fi
+done
--- unmount.sh
+++ unmount.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 
-if [ $# != 1 ]; then
-	echo "error!!"
-	echo "Usage: ntfsunmount [disk_name]"
-	exit -1
-fi
+count=0
+for arg in `diskutil info -all | grep 'Volume Name' | cut -d : -f 2 | sed 's/^[[:space:]]*//' | sed 's/[ \t]*$//g'`
+do
+  volume_names[$count]=$arg
+  count=`expr $count + 1`
+done
 
-disk_name="$1"
-device_node=`diskutil info ${disk_name} | grep 'Device Node' | cut -d : -f 2 | sed 's/^[[:space:]]*//' | sed 's/[ \t]*$//g'`
 
-mounted_path="/Volumes/${disk_name}"
+#if [ $# != 1 ]; then
+#	echo "error!!"
+#	echo "Usage: ntfsunmount [disk_name]"
+#	exit -1
+#fi
 
-sudo diskutil unmount "${mounted_path}"
+tLen=${#volume_names[@]}
+echo $tLen
 
-link_path=~/Desktop/${disk_name}
-echo ${link_path}
-if [ ! -L ${link_path} ]; then
-	echo "${link_path} not exist"
-	exit 0
-else
-	echo "remove link file"
-	rm ${link_path}
-	exit 0
-fi
+for (( i=0; i<${tLen}; i++ ));
+do
+	echo ${volume_names[$i]}
+    disk_name=${volume_names[$i]}
+	device_node=`diskutil info ${disk_name} | grep 'Device Node' | cut -d : -f 2 | sed 's/^[[:space:]]*//' | sed 's/[ \t]*$//g'`
+
+	mounted_path="/Volumes/${disk_name}"
+
+	sudo diskutil unmount "${mounted_path}"
+
+	link_path=~/Desktop/${disk_name}
+	echo ${link_path}
+	if [ ! -L ${link_path} ]; then
+		echo "link path ${link_path} not exist"
+		#exit 0
+		continue
+	else
+		echo "remove link file"
+		rm ${link_path}
+		#exit 0
+		continue
+	fi
+done
